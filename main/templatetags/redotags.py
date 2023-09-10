5e5db368daaaedc34a84ea645517af7e7b63ccff
Forever Inc dev <redodevs@gmail.com>
2023-09-10 07:12:05 +0300

    save

--- main/templatetags/redotags.py
+++ main/templatetags/redotags.py
@@ -16,3 +16,7 @@ def redo_holder(field,placeholder):
         field.field.widget.attrs['placeholder']=placeholder
         
     return field
+
+@register.filter
+def is_input(field):
+    field.field.widget.attrs['class']="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
