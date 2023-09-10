fbdb67d393c1da9a365e74431dd09ebde9dfc9fa
Forever Inc dev <redodevs@gmail.com>
2023-09-10 07:56:31 +0300

    save

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import index
+from django.urls import path, re_path
+from .views import index,service_worker,manifest,offline,robots,sitemap
 
 
 urlpatterns=[
-    path('',index,name='home')
+    path('',index,name='home'),
+    re_path(r'^serviceworker\.js$', service_worker, name='sw'),
+    re_path(r'^manifest\.json$', manifest, name='manifest'),
+    path('offline/',offline,name='offline'),
+    path('sitemap/',sitemap,name='sitemap'),
+    re_path(r'^robots\.txt$', robots, name='robots'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 from .utils import seo
+from django.http import HttpResponse
+import os
+from pathlib import Path
+
+
+
+def robots(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    path=os.path.join(BASE_DIR,'templates','base','robots.txt')
+    response=HttpResponse(open(path).read(),content_type='application/text')
+    return response
+
+def sitemap(request):
+    return render(request,'base/sitemap.xml')
+
+def custom_404_view(request,exception):
+    return render(request, 'base/error.html', status=404)
+
+def custom_500_view(request):
+    return render(request, 'base/505.html', status=500)
+
+def service_worker(request):
+    BASE_DIR = Path(__file__).resolve().parent.parent
+    path=os.path.join(BASE_DIR,'templates','base','sw.js')
+    response=HttpResponse(open(path).read(),content_type='application/javascript')
+    return response
+
+def manifest(request):
+    return render(request, 'base/manifest.json', content_type='application/json')
+    
+def offline(request):
+    return render(request,'main/offline.html' )   
 
 
 
