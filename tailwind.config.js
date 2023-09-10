5b2cf3a4a6960d92d3d696aebafa496231f6ef81
Forever Inc dev <redodevs@gmail.com>
2023-09-10 08:08:22 +0300

    save

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+	darkMode: "class",
+
 	content: ["./templates/**/*.html", "./node_modules/flowbite/**/*.js"],
 	theme: {
 		extend: {},
