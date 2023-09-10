7bfc9973b6dc729df123e89db3b80a888f24f89b
Forever Inc dev <redodevs@gmail.com>
2023-09-10 08:28:27 +0300

    save

--- base/settings.py
+++ base/settings.py
@@ -12,8 +12,6 @@ SECRET_KEY = get_random_secret_key()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
@@ -22,6 +20,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG','False').lower()=='true'
 
 ALLOWED_HOSTS = ["*"]
+
+AUTH_USER_MODEL = "accounts.Account"
 
 
 # Application definition
@@ -36,7 +36,9 @@ INSTALLED_APPS = [
     'main',
     "debug_toolbar",
     "django_htmx",
-
+    'accounts',
+    'cloudinary_storage',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -101,18 +103,11 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
@@ -154,13 +149,13 @@ else:
     }
     }
 
-    # CLOUDINARY_STORAGE={
-    #     'CLOUD_NAME': 'dnb8rethz',
-    #     'API_KEY': os.environ.get('API_KEY'),
-    #     'API_SECRET': os.environ.get('API_SECRET')
-    # }
+    CLOUDINARY_STORAGE={
+        'CLOUD_NAME': 'dnb8rethz',
+        'API_KEY': '123456789',
+        'API_SECRET': os.environ.get('API_SECRET')
+    }
     
-    # DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
+    DEFAULT_FILE_STORAGE='cloudinary_storage.storage.MediaCloudinaryStorage'
 
 
     
