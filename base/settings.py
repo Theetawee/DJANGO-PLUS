c40ac68455fdf0819787e06351edcce0a73bc88a
Forever Inc dev <redodevs@gmail.com>
2023-10-19 10:29:47 +0300

    save

--- base/settings.py
+++ base/settings.py
@@ -13,10 +13,8 @@ SECRET_KEY = get_random_secret_key()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = ["*"]
--- main/templatetags/redotags.py
+++ main/templatetags/redotags.py
@@ -1,5 +1,4 @@
 from django import template
-from django.utils.safestring import mark_safe
 
 register=template.Library()
 
@@ -26,54 +25,3 @@ def is_input(field):
  
  
     
-@register.simple_tag
-def seo_tags(meta_title, meta_description, follow='follow'):
-    # Escape any HTML in the meta_title and meta_description
-    meta_title = meta_title.strip()
-    meta_description = meta_description.strip()
-    
-    # Define a dictionary to map follow values to robots meta tag content
-    robot_tags = {
-        'follow': 'index, follow, max-snippet:-1, max-video-preview:-1',
-        'nofollow': 'noindex, nofollow',
-    }
-    
-    # Use the dictionary to get the appropriate robot tag content
-    robot_content = robot_tags.get(follow, robot_tags['follow'])
-    
-    html = f'<title>{meta_title}</title><meta name="description" content="{meta_description}">'
-    html += f'<meta name="robots" content="{robot_content}" />'
-    
-    return mark_safe(html)
-
-
-@register.simple_tag
-def twitter_card(title, description, image_url):
-    title = title.strip()
-    description = description.strip()
-    image_url = image_url.strip()
-
-    html = f'<meta name="twitter:card" content="summary" />' \
-           f'<meta name="twitter:title" content="{title}" />' \
-           f'<meta name="twitter:description" content="{description}" />' \
-           f'<meta name="twitter:image" content="{image_url}" />' \
-           '<meta name="twitter:image:alt" content="Image Alt Text" />'\
-            '<meta name="twitter:site" content="@redodevs" />'
-   
-
-    return mark_safe(html)
-
-
-@register.simple_tag
-def og_tags(title, description, image_url, og_type='website'):
-    title = title.strip()
-    description = description.strip()
-    image_url = image_url.strip()
-
-
-    html = f'<meta property="og:title" content="{title}" />' \
-           f'<meta property="og:description" content="{description}" />' \
-           f'<meta property="og:image" content="{image_url}" />' \
-           f'<meta property="og:type" content="{og_type}" />' \
-
-    return mark_safe(html)
