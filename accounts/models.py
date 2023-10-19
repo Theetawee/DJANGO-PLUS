6d80e7a398271f723143c524f0a117fe283d8c88
Forever Inc dev <redodevs@gmail.com>
2023-10-19 11:17:34 +0300

    save

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from phonenumber_field.modelfields import PhoneNumberField
-from django.utils import timezone
 from django.conf import settings
 import os
 
