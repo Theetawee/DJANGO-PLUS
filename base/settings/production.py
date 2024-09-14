80ae2b88bcbf067bc1f5c7bae07e13c2e2677c84
Pattawee Drake Khaotungkulmethee <140291119+Theetawee@users.noreply.github.com>
2024-09-14 16:10:29 +0100

    Update production.py

--- base/settings/production.py
+++ base/settings/production.py
@@ -19,6 +19,47 @@ DATABASES = {
     }
 }
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {
+            "level": "WARNING",  # Display warnings and above in the console
+            "class": "logging.StreamHandler",
+            "formatter": "simple",
+        },
+        "mail_admins": {
+            "level": "WARNING",  # Send warnings and errors to admins via email
+            "class": "django.utils.log.AdminEmailHandler",
+        },
+    },
+    "formatters": {
+        "simple": {
+            "format": "[{asctime}] {levelname} {message}",
+            "style": "{",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": [
+                "console",
+                "mail_admins",
+            ],  # Send logs to both console and admins
+            "level": "WARNING",  # Set to warning level and above
+            "propagate": True,
+        },
+        "django.request": {
+            "handlers": ["mail_admins"],  # Critical request errors go to admins
+            "level": "ERROR",  # Only log errors for requests
+            "propagate": False,
+        },
+    },
+}
+
+ADMINS = [
+    ("Tawee", "tawee@waanverse.com"),
+]
+
 STATIC_URL = "https://your_cdn_url/"
 
 BACKUP_DIRECTORY = os.path.join(BASE_DIR, "backups/production")
